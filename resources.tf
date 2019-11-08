4aeb0996aab6d043278d83d0ff4c46797ae1ac42
ronthedistance <ron.revilla.30@my.csun.edu>
2019-11-08 09:26:27 -0800

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 # create a VPC
 resource "aws_vpc" "project1-vpc" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "172.16.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
