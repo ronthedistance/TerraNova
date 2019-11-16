450b9810cf94cae4c2e4e73686f5dbfad17cb244
ronthedistance <ron.revilla.30@my.csun.edu>
2019-11-16 12:10:57 -0800

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -144,7 +144,7 @@ resource "aws_security_group" "private-security-group" {
     to_port   = 22
     protocol  = "tcp"
 
-    # cidr_blocks = ["0.0.0.0/0"] # no CIDR block, specify subnet instead
+    cidr_blocks = ["10.0.0.16"] # no CIDR block, specify subnet instead
     # Allow incoming connections only from public subnet
     security_groups = ["${aws_security_group.public-security-group.id}"]
   }
