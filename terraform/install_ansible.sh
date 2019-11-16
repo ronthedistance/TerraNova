675ac387c7f9e73f623f7e5965a322ad885efb84
cacaocat-syr <54485787+cacaocat-syr@users.noreply.github.com>
2019-11-16 12:19:40 -0800

    Update install_ansible.sh

--- terraform/install_ansible.sh
+++ terraform/install_ansible.sh
@@ -4,4 +4,4 @@ sudo apt-get install python -y
 sudo apt-get install software-properties-common -y
 sudo apt-get-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible -y
-#<append your keys to the authorized_keys file>
+#<append your extra keys to the authorized_keys file>
